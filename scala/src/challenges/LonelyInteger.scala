0ae660ffd6d0d172a9455a599b34a7167f7c835d
Tomas Alabes <tomas.alabes@gmail.com>
2013-10-29 22:47:52 -0300

    Initial commit

---
+++ scala/src/challenges/LonelyInteger.scala
@@ -0,0 +1,54 @@
+import scala.collection.mutable
+
+/*
+
+There are N integers in an array A. All but one integer occurs in pairs. Your task is to find out that number that occurs only once.
+
+Input Format
+
+The first line of the input contains an integer N indicating N integers in the array A. Next line contains N integers each separated by a single space.
+
+Constraints
+
+1 <= N < 100
+N % 2 = 1 ( N is an odd number )
+0 <= i <= 100, ∀ i ∈ A
+
+Output Format
+
+Output (S) which is the number that occurs only once.
+
+Example:
+
+Input:
+3
+5 5 4
+
+Output: (the lonely integer)
+4
+
+*/
+
+object Solution {
+
+  def main(args: Array[String]) {
+    val size: String = readLine()
+    val numbers: String = readLine()
+
+    val map = mutable.HashMap[Int, Boolean]()
+    val array: Array[String] = numbers.split(" ")
+
+    for (x <- array) {
+      val number:Int = Integer.valueOf(x)
+      if (map.contains(number)) {
+        map.put(number, true)
+      } else {
+        map.put(number, false)
+      }
+    }
+
+    val solution: Int = map.find(_._2 == false).get._1
+    println(solution)
+  }
+
+}
