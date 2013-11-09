187a0d009f7acc8c08177db4f0528043cf06b0f2
Tomas Alabes <tomas.alabes@gmail.com>
2013-11-09 15:25:58 -0300

    Better ruby test

--- ruby/test/challenges/cracking/linkedLists/NodeTest.rb
+++ ruby/test/challenges/cracking/linkedLists/NodeTest.rb
@@ -4,35 +4,44 @@ require '../../../../src/challenges/cracking/linkedLists/Node'
 
 class NodeTest < Test::Unit::TestCase
 
-  def test_all
+  def setup
+    @n1 = Node.new(1)
+    @n1.append_to_tail 3
+    @n1.append_to_tail 2
+    @n1.append_to_tail 2
+    @n1.append_to_tail 4
+    @n1.append_to_tail 5
+  end
 
-    n1 = Node.new(1)
-    n1.append_to_tail 3
-    n1.append_to_tail 2
-    n1.append_to_tail 2
-    n1.append_to_tail 4
-    n1.append_to_tail 5
+  def test_get_size
+    assert_equal(6, @n1.size)
+  end
 
-    assert_equal(6, n1.size)
+  def test_delete_node
+    @n1.delete_node 3 # 1-2-2-4-5
 
-    n1.delete_node 3 # 1-2-2-4-5
+    assert_not_equal(@n1.next.data, 3)
+    assert_equal(5, @n1.size)
+  end
 
-    assert_not_equal(n1.next.data, 3)
-    assert_equal(5, n1.size)
+  def test_remove_duplicates
+    @n1.remove_duplicates() # 1-3-2-4-5
 
-    n1.remove_duplicates() # 1-2-4-5
+    assert_equal(5, @n1.size)
+  end
 
-    assert_equal(4, n1.size)
-
-    last_two = n1.nth_to_last(2) # 4-5
+  def test_nth_to_last
+    last_two = @n1.nth_to_last(2) # 4-5
     assert_equal(2, last_two.size)
     assert_equal(last_two.data, 4)
     assert_equal(last_two.next.data, 5)
+  end
 
-    n1.delete # 2-4-5
+  def test_delete
+    @n1.delete
 
-    assert_equal(3, n1.size)
-    assert_equal(2, n1.data)
+    assert_equal(5, @n1.size)
+    assert_equal(3, @n1.data)
 
   end
 
